ca3733f7d7047561dec1e4ebf181172880b69e3a
bipedal.robot <malgorzata.wicha@gmail.com>
2024-07-12 17:08:49 +0200

    Add urdf files of solo quadruped robot
    Moreover - add gazebo and rviz launch files.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # robotics-solo-quadruped-robot
+
+Solo 8 quadruped robot is open-sourced by the Open Dynamic Robot Initiative. See: https://open-dynamic-robot-initiative.github.io/.
+
+This is the final project of the course:  ROS2 Control Framework by The Construct. See: the.construct.ai.
+
+The goal is to move the legs of the robot in a specific way using ros2_control.
+
+## Source of the urdf model
+https://github.com/open-dynamic-robot-initiative/robot_properties_solo/tree/master
+
+## How to run
+
+### Build repository
+
+```
+cd robotics-solo-quadruped-robot
+colcon build
+source install/setup.bash
+```
+
+### Display in RViz
+```
+ros2 launch solo_description display.launch.py
+```
+
+### Run in gazebo
+```
+ros2 launch solo_description gazebo.launch.py
+```
---
+++ src/solo_description/launch/display.launch.py
@@ -0,0 +1,49 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    model_arg = DeclareLaunchArgument(
+        name="model",
+        default_value=os.path.join(
+            get_package_share_directory("solo_description"),
+            "urdf",
+            "solo8.urdf.xacro"
+        ),
+        description="Absolute path to the robot URDF file"
+    )
+
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]))
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{
+            "robot_description": robot_description
+        }]
+    )
+
+    joint_state_publisher_gui = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui"
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen", # see all logs in terminal
+        arguments=["-d", os.path.join(get_package_share_directory("solo_description"), "rviz", "display.rviz")]
+    )
+
+    # Contains list of all nodes that we want to launch
+    return LaunchDescription([
+        model_arg,
+        robot_state_publisher,
+        joint_state_publisher_gui,
+        rviz_node,
+    ])
